b1af5016b6bbb44efb694b66f498950a6e5e9177
Cheng Gong <cheng.gong@it.uu.se>
2018-01-22 16:05:14 +0100

    Heat inverse/forward solving

--- testheat.m
+++ testheat.m
@@ -2,14 +2,19 @@ clear
 close all
 
 %% Initialize
-
 % Settings
 interpOption = 'linear';
+yearIndex = 4;
 
-% Load the data set
+%% Load data
 load('LF_4_aver.mat');
-data = LF{1,1}.T;
-dIndex = 4;
+load('densityData.mat');
+
+%%
+data = LF{yearIndex}.T;
+rho = rhoData{yearIndex};
+
+dIndex = 0;
 
 % Measurements
 % Time vector (row)
@@ -20,19 +25,45 @@ z_data = data.z_a;
 % Temperature
 T_data = data.T_a;
 % T_data = data.T_corr{dIndex};
-    
-% Add noise
-noise = 0.2*(1-2*rand(size(T_data)));
-T_data = T_data + noise;
+
+% % Add noise
+% noise = 0.0*(1-2*rand(size(T_data)));
+% T_data = T_data + noise;
 
 % Physical parameters
-rho = 900;
 C = 152.5 + 7.122 * (273.15 - 10);
 
+
+%% Initialize
+Nk = 5;
+zK = linspace(0, 12, Nk)';
+K0 = 0.4e5*ones(Nk, 1);
+
+% Time discretization same size as measurment
+Nt = length(t_data);
+
+% cut the data according to the range of K
+[T_data, z_data] = cutData(T_data, z_data, [zK(1),zK(end)]);
+
+%% Optimize for the averaged value 
+Nz = length(z_data);
+K_opt = inverseK(data, 0, zK, K0, Nz, rho);
+% K_opt = K0;
+
+%% Solve Heat equation
+
+% Set initial and boundary conditions
+[Tbc, T0, z, t, dz, dt] = setIBCs(z_data, t_data, Nz, Nt, T_data, interpOption);
+
+% Set Parameters for solving
+heatParam = setHeatParam(dt, Nt, dz, Nz, rho, C, T0, Tbc.Up, Tbc.Down, zK);
+
+[T_sol] = solveHeat(t_data, z_data, K_opt, heatParam);
+
+%% Visualize measurement
 % mesh for measurment
 [X_data, Y_data] = meshgrid(t_data, z_data);
 
-%% Visualize measurement
 figure
 subplot(2, 1, 1)
 surf(X_data, Y_data, T_data)
@@ -44,62 +75,8 @@ axis tight
 caxis([-20, -2]);
 grid off
 
-%% Solve Heat equation
-% Spactial Discretization
-Nz = 121;
-z = linspace(min(z_data), max(z_data),Nz)';
-dz = abs(z(2) - z(1));
-
-% Time discretization same size as measurment
-Nt = length(t_data);
-t = linspace(min(t_data), max(t_data),Nt);
-dt = abs(t(2) - t(1));
-
-% Mesh for solving PDE
-[X, Y] = meshgrid(t, z);
-
-% Initial Heat conductivity
-Nk = 5;
-zK = linspace(0, 12, Nk)';
-Kp = 0.4e6*ones(Nk, 1);
-
-
-% Interpolate initial condition
-T0 = interp1(z_data, T_data(:,1), z, interpOption);
-
-% Interpolate Boundary conditions
-TbcUp = interp1(t_data, T_data(1,:), t, interpOption);
-TbcDown = interp1(t_data, T_data(end,:), t, interpOption);
-
-heatParam = setHeatParam(dt, Nt, dz, Nz, rho, C, T0, TbcUp, TbcDown, zK);
-
-% Project data to the computational domain
-f_data = projectY2D(T_data, t_data, z_data, t, z);
-
-%% Plot
-% subplot(2,1,2)
-% surf(X,Y,f_data)
-% view(2)
-% shading interp;
-% colorbar
-% colormap(jet)
-% axis tight
-% caxis([-20,0]);
-% grid off
-
-
-%% Optimisation
-
-f = @(K) objF(K, @solveHeat, heatParam, z, t, f_data);
-
-options = optimoptions('lsqnonlin','Display','iter','typicalX', Kp,'TolFun',1e-10,'TolX',1e-10);
-[K_opt,resnorm,residual,exitflag,output] = lsqnonlin(f, Kp, [], [],options);
-
-%% Plot optimal solution
-[T_sol] = solveHeat(t, z, K_opt, heatParam);
-
 subplot(2,1,2)
-surf(X,Y,T_sol)
+surf(X_data,Y_data,T_sol)
 view(2)
 shading interp;
 colorbar
