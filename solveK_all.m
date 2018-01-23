f6676ae4ff6ef5f5767c3c39a5ebfccbafc7265b
Cheng Gong <cheng.gong@it.uu.se>
2018-01-23 14:29:49 +0100

    add automatical way to find K for data2012-2015

---
+++ solveK_all.m
@@ -0,0 +1,25 @@
+clear 
+close all
+
+%%
+yearIndex = 1:4;
+K_opt = {};
+
+for i = 1: length(yearIndex)
+    K_opt{i} = testheat(yearIndex(i));
+end
+
+%%
+K_mat = cell2mat(K_opt) ./ 24./3600;
+z = linspace(0,10,5);
+
+figure
+for i = 1: length(yearIndex)
+    plot(z , K_mat(:, i))
+    hold on
+end
+xlim([0, 9])
+ylim([0, 3.5])
+xlabel('z')
+ylabel('K')
+legend({'2012', '2013', '2014', '2015'})
--- testheat.m
+++ testheat.m
@@ -1,84 +1,89 @@
-clear
-close all
+function K_opt = testheat(yearIndex)
+    %% Initialize
+    % Settings
+    interpOption = 'linear';
 
-%% Initialize
-% Settings
-interpOption = 'linear';
-yearIndex = 2;
+    %% Load data
+    load('LF_4_aver.mat');
+    load('densityData.mat');
 
-%% Load data
-load('LF_4_aver.mat');
-load('densityData.mat');
+    %%
+    data = LF{yearIndex}.T;
+    rho = rhoData{yearIndex};
 
-%%
-data = LF{yearIndex}.T;
-rho = rhoData{yearIndex};
+    dIndex = 0;
 
-dIndex = 0;
+    % Measurements
+    % Time vector (row)
+    t_data = data.t';
+    % Position vector (column)
+    z_data = data.z_a;
+    % z_data = data.z_corr{dIndex};
+    % Temperature
+    T_data = data.T_a;
+    % T_data = data.T_corr{dIndex};
 
-% Measurements
-% Time vector (row)
-t_data = data.t';
-% Position vector (column)
-z_data = data.z_a;
-% z_data = data.z_corr{dIndex};
-% Temperature
-T_data = data.T_a;
-% T_data = data.T_corr{dIndex};
+    % % Add noise
+    % noise = 0.0*(1-2*rand(size(T_data)));
+    % T_data = T_data + noise;
 
-% % Add noise
-% noise = 0.0*(1-2*rand(size(T_data)));
-% T_data = T_data + noise;
-
-% Physical parameters
-C = 152.5 + 7.122 * (273.15 - 10);
+    % Physical parameters
+    C = 152.5 + 7.122 * (273.15 - 10);
 
 
-%% Initialize
-Nk = 5;
-zK = linspace(0, 12, Nk)';
-K0 = 0.4e5*ones(Nk, 1);
+    %% Initialize
+    Nk = 5;
+    zK = linspace(0, 10, Nk)';
+    K0 = 0.4e5*ones(Nk, 1);
 
-% cut the data according to the range of K
-[T_data, z_data] = cutData(T_data, z_data, [zK(1),zK(end)]);
+    % cut the data according to the range of K
+    [T_data, z_data] = cutData(T_data, z_data, [zK(1),zK(end)]);
 
-%% Optimize for the averaged value 
-Nz = length(z_data);
-K_opt = inverseK(data, 0, zK, K0, Nz, rho);
-% K_opt = K0;
+    %% Optimize for the averaged value
+    Nz = length(z_data);
+    K_opt = inverseK(data, 0, zK, K0, Nz, rho);
 
-%% Solve Heat equation
+    %% Solve Heat equation
 
-% Set initial and boundary conditions
-[Tbc, T0, z, t, dz, Nt, dt] = setIBCs(z_data, t_data, Nz, T_data, interpOption);
+    % Set initial and boundary conditions
+    [Tbc, T0, z, t, dz, Nt, dt] = setIBCs(z_data, t_data, Nz, T_data, interpOption);
 
-% Set Parameters for solving
-heatParam = setHeatParam(dt, Nt, dz, Nz, rho, C, T0, Tbc.Up, Tbc.Down, zK);
+    % Set Parameters for solving
+    heatParam = setHeatParam(dt, Nt, dz, Nz, rho, C, T0, Tbc.Up, Tbc.Down, zK);
 
-[T_sol] = solveHeat(t, z, K_opt, heatParam);
+    [T_sol] = solveHeat(t, z, K_opt, heatParam);
 
-%% Visualize measurement
-% mesh for measurment
-[X_data, Y_data] = meshgrid(t_data, z_data);
+    %% Visualize measurement
+    % mesh for measurment
+    [X_data, Y_data] = meshgrid(t_data, z_data);
 
-figure
-subplot(2, 1, 1)
-surf(X_data, Y_data, T_data)
-view(2)
-shading interp;
-colorbar
-colormap(jet)
-axis tight
-caxis([-20, -2]);
-grid off
+    figure
+    subplot(2, 1, 1)
+    surf(X_data, Y_data, T_data)
+    view(2)
+    shading interp;
+    colorbar
+    colormap(jet)
+    axis tight
+    caxis([-20, -2]);
+    xlabel('t')
+    ylabel('z')
+    title(['Measurements at 201', num2str(yearIndex+1)]);
+    grid off
+    
 
-[X, Y] = meshgrid(t, z);
-subplot(2,1,2)
-surf(X,Y,T_sol)
-view(2)
-shading interp;
-colorbar
-colormap(jet)
-axis tight
-caxis([-20,-2]);
-grid off
+    [X, Y] = meshgrid(t, z);
+    subplot(2,1,2)
+    surf(X,Y,T_sol)
+    view(2)
+    shading interp;
+    colorbar
+    colormap(jet)
+    axis tight
+    caxis([-20,-2]);
+    xlabel('t')
+    ylabel('z')
+    title(['Optimal solution at 201', num2str(yearIndex+1)]);
+    grid off
+
+end
