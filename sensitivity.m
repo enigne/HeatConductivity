8856872cfe92715ee174064afa6e5fb376acffd3
Cheng Gong <cheng.gong@it.uu.se>
2018-01-25 14:34:07 +0100

    save sensitivity before test for compare computations to data and the measured data point only

--- sensitivity.m
+++ sensitivity.m
@@ -3,87 +3,103 @@ close all;
 %% Initialize
 % Settings
 interpOption = 'linear';
+yearIndex = 1;
 
-% Load optimal K
-% load('invK_1e8_Nz241.mat');
-% load('invK_1e8_Nz601.mat');
-load('invK_0_8.mat');
-K0 = K_opt_ave(:,end);
+%% Load data
+load('LF_4_aver.mat');
+load('densityData.mat');
+load('invK_realRho.mat');
+
+data = LF{yearIndex}.T;
+rho = rhoData{yearIndex};
+K0 = z_05_8.K_opt{yearIndex};
+
 % Initial Heat conductivity
 Nk = length(K0);
-zK = linspace(0, 8, Nk)';
-
-% Load the data set
-load('LF_4_aver.mat');
-data = LF{1,1}.T;
-
-% Load average and mean
-load('ML_data.mat');
 
 % Measurements
-% Time vector (row)
-t_data = data.t';
-% Position vector (column)
-z_data = data.z_a;
-% Temperature
-T_data = data.T_a;
+[t_data, z_data, T_data] = loadData(data);
+
+% z-coordinates of K
+zK = linspace(0.5, 8, Nk)';
 
 % cut the data according to the range of K
 [T_data, z_data] = cutData(T_data, z_data, [zK(1),zK(end)]);
-[T_E, ~] = cutData(T_E, z_data, [zK(1),zK(end)]);
-[T_S, ~] = cutData(T_S, z_data, [zK(1),zK(end)]);
 
-% mask for T < -2
-mask = find( T_data < 20 );
+% % Load average and mean
+% load('ML_data.mat');
+% [T_E, ~] = cutData(T_E, z_data, [zK(1),zK(end)]);
+% [T_S, ~] = cutData(T_S, z_data, [zK(1),zK(end)]);
+
+% mask for T >= -2 put 0
+mask = find( T_data >= -2);
 
 % Physical parameters
-rho = 900;
 C = 152.5 + 7.122 * (273.15 - 10);
 
 %% Compute dTdz
-dZfine = 5;
-[dTdz, matDTDz] = computeDTDz(z_data, t_data, T_data, dZfine, zK, K0, rho, C, mask, interpOption);
+dZfine = 20;
+[dTdz, matDTDz, ~, t] = computeDTDz(z_data, t_data, T_data, dZfine, zK, K0, rho, C, mask, interpOption);
 
 %% Compute A  
 dZfine = 10;
 dK = 0.001;
-AK = computeKSensitivity(z_data, t_data, T_data, dZfine, zK, K0, dK, rho, C, mask, interpOption);
-B = inv(AK'*AK);
-%%
+A = computeKSensitivity(z_data, t_data, T_data, dZfine, zK, K0, dK, rho, C, mask, interpOption);
+B = A'*A;
+AK = B \ A';
+
+%% Plot AK and A
+figure
+[X_data, Y_data] = meshgrid(t, z_data);
+for i = 1 : 6
+    if i > 1
+        p_data = reshape(AK(i-1,:), size(X_data));
+        subTitle = ['K',num2str(i-1)];
+    else
+        p_data = project2D(T_data, t_data, z_data, t, z_data);
+        subTitle = ['Measurements in 201',num2str(yearIndex+1)];
+    end
+    
+    p_data(mask) = nan;
+    
+    subplot(3, 2, i)
+    surf(X_data, Y_data, p_data);
+    view(2)
+    shading interp;
+    colorbar
+    colormap(jet)
+    axis tight
+    title(subTitle);
+    if i == 1
+        caxis([-20, -2]);
+    end
+    grid off
+end
+
+%% Plot Az and Az*R
 figure
 zALeg = {};
 for i = 1: Nk
     zALeg{i} = ['K', num2str(i)];
 end
-Az = B * AK' * matDTDz;
+Az = B \ A' * matDTDz;
 
 Nz = length(z_data);
 R = tril(ones(Nz));
 D = Az*R;
 subplot(2,1,1)
 plot(z_data, Az');
-xlim([0, max(z_data)])
+xlim([min(z_data), max(z_data)])
 % legend(zALeg);
 xlabel('z');
 ylabel('Az');
 subplot(2,1,2)
 plot(z_data, D');
-xlim([0, max(z_data)])
+xlim([min(z_data), max(z_data)])
 xlabel('z');
 ylabel('Az*R');
 legend(zALeg);
 
-
-%% Compute rho
-dZfine = 5;
-dRho = 0.001;
-NRho = 5;
-rho = 900 * ones(NRho,1);
-zRho = linspace(0, 8, NRho)';
-
-ARho = computeRhoSensitivity(z_data, t_data, T_data, dZfine, zK, K0, dRho, rho, zRho, C, mask, interpOption);
-%%
-D = - B * AK' * ARho
 
 %% compute weight
 w = 1./ (T_S.^(0.5));
@@ -91,32 +107,34 @@ W = w(mask);
 weightedAK = AK' * spvardiag(W);
 weightB = inv(weightedAK * AK);
 
-% % %% Visualize measurement
+% %% Compute rho
+% dZfine = 5;
+% dRho = 0.001;
+% NRho = 5;
+% rho = 900 * ones(NRho,1);
+% zRho = linspace(0, 8, NRho)';
+% 
+% ARho = computeRhoSensitivity(z_data, t_data, T_data, dZfine, zK, K0, dRho, rho, zRho, C, mask, interpOption);
+% %%
+% D = - B * AK' * ARho
+
+%% Visualize measurement
 % mesh for measurment
-[X_data, Y_data] = meshgrid(t_data, z_data);
+% [X_data, Y_data] = meshgrid(t, z_data);
+% 
+% figure
+% % indicator = 1.* (T_data < -2);
+% % subplot(2, 1, 1)
+% surf(X_data, Y_data, dTdz)
+% view(2)
+% shading interp;
+% colorbar
+% colormap(jet)
+% axis tight
+% caxis([-2, 2]);
+% grid off
 
-figure3
-indicator = 1.* (T_data < -2);
-subplot(2, 1, 1)
-surf(X_data, Y_data, w.*indicator)
-view(2)
-shading interp;
-colorbar
-colormap(jet)
-axis tight
-% caxis([-20, -2]);
-grid off
-
-
-subplot(2, 1, 2)
+% 
+% subplot(2, 1, 2)
 
 %%
-figure
-surf(X_data, Y_data,  reshape(AK(:,4),size(X_data)))
-view(2)
-shading interp;
-colorbar
-colormap(jet)
-axis tight
-% caxis([-1, 1]);
-grid off
