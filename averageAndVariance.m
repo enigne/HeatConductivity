ec2e7952bd16850c827ddbf2330193bd2b62d264
Cheng Gong <cheng.gong@it.uu.se>
2018-01-26 11:01:41 +0100

    compute mean values from data, should equal to T_a, and the variances

--- averageAndVariance.m
+++ averageAndVariance.m
@@ -1,10 +1,12 @@
 clear
 close all
 
+%%
+yearIndex = 3;
 %% Load data
 load('LF_4_aver.mat');
-data = LF{1,1}.T;
-dataIndex = [1:9];
+data = LF{yearIndex}.T;
+dataIndex = [1: 9];
 nInd = length(dataIndex);
 
 %%
@@ -20,12 +22,12 @@ T_E = zeros(size(T_a));
 
 for i = 1 : nInd
     % Position vector (column)
-    z_data = data.z_corr{dataIndex(i)};
+    z_data = data.z_i{dataIndex(i)};
     % Temperature
-    T_data = data.T_corr{dataIndex(i)};
+    T_data = data.T_i{dataIndex(i)};
     
     % Project data to the computational domain
-    f_data = projectY2D(T_data, t, z_data, t, z_a);
+    f_data = project2D(T_data, t, z_data, t, z_a);
     
     res = (f_data - T_a) .^ 2;
     
@@ -35,3 +37,18 @@ end
 
 T_E = T_E / nInd;
 T_S = T_S / (nInd - 1);
+
+
+%% 
+[X_data, Y_data] = meshgrid(t, z_a);
+surf(X_data, Y_data, T_E-T_a);
+view(2)
+shading interp;
+colorbar
+colormap(jet)
+axis tight
+
+%%
+dataS{yearIndex}.T_S = T_S;
+dataS{yearIndex}.T_E = T_E;
+
