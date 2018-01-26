4c6a9f20716034c99e55b6dc22b54f9e52fe76c0
Cheng Gong <cheng.gong@it.uu.se>
2018-01-26 12:36:01 +0100

    add output for nan indeces

--- cutNan.m
+++ cutNan.m
@@ -1,4 +1,4 @@
-function [T_data, t_data] = cutNan(T_data, t_data)
+function [T_data, t_data, noNan] = cutNan(T_data, t_data)
     flagNan = sum(isnan(T_data), 1);
     noNan = (flagNan == 0);
     T_data = T_data(:, noNan);
