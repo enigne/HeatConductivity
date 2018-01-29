400a683da067fc44f3aea5d0e722d38c0f9a26d5
Cheng Gong <cheng.gong@it.uu.se>
2018-01-29 12:24:54 +0100

    add output index after cutting

--- cutData.m
+++ cutData.m
@@ -1,6 +1,8 @@
-function [T_data, z_data] = cutData(T_data, z_data, z)
+function [T_data, z_data, indCutZ] = cutData(T_data, z_data, z)
     zminInd = find(z_data >= z(1), 1, 'first');
     zmaxInd = find(z_data <= z(2), 1, 'last');
-    z_data = z_data(zminInd:zmaxInd);
-    T_data = T_data(zminInd:zmaxInd, :);
+    
+    indCutZ = zminInd:zmaxInd;
+    z_data = z_data(indCutZ);
+    T_data = T_data(indCutZ, :);
 end
