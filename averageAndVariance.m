1f188ac889f796fe3c2e426fbfcac6529663d9f4
Cheng Gong <cheng.gong@it.uu.se>
2018-01-29 12:26:03 +0100

    calculate sd for 2012 2014 works fine

--- averageAndVariance.m
+++ averageAndVariance.m
@@ -2,7 +2,7 @@ clear
 close all
 
 %%
-yearIndex = 3;
+yearIndex = 2;
 %% Load data
 load('LF_4_aver.mat');
 data = LF{yearIndex}.T;
@@ -26,31 +26,44 @@ for i = 1 : nInd
     % Temperature
     T_data = data.T_i{dataIndex(i)};
     
+    [T_data, tcut, noNanInd] = cutNan(T_data, t);
     % Project data to the computational domain
-%     f_data = project2D(T_data, t, z_data, t, z_a);
+%     f_data = project2D(T_data, tcut, z_data, t, z_a);
     
     f_data = project2DY(T_data, z_data, z_a);
     
-    res = (f_data - T_a) .^ 2;
+    res = (f_data - T_a(:, noNanInd)) .^ 2;
     
-    T_E = T_E + f_data;
-    T_S = T_S + res;
+%     T_E = T_E + f_data;
+    T_S(:, noNanInd) = T_S(:, noNanInd) + res;
 end
 
-T_E = T_E / (nInd);
+% T_E = T_E / (nInd);
 T_S = T_S / (nInd - 1);
 
 
 %% 
 [X_data, Y_data] = meshgrid(t, z_a);
-surf(X_data, Y_data, T_E-T_a);
+figure
+subplot(2,1,1)
+surf(X_data, Y_data, data.T_sd );
 view(2)
 shading interp;
 colorbar
 colormap(jet)
 axis tight
-% caxis([-1e-10, 1e-10])
+% caxis([-1e-14, 1e-14])
+caxis([0, 2.5])
+
+subplot(2,1,2)
+surf(X_data, Y_data, T_S.^0.5);
+view(2)
+shading interp;
+colorbar
+colormap(jet)
+axis tight
+caxis([0, 2.5])
 %%
 dataS{yearIndex}.T_S = T_S;
-dataS{yearIndex}.T_E = T_E;
+dataS{yearIndex}.T_E = T_a;
 
