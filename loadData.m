73b56d02d32f780102cfec9af5ab17ac4fbf4e3d
Cheng Gong <cheng.gong@it.uu.se>
2018-02-01 19:29:13 +0100

    cut time when loading, return time index

--- loadData.m
+++ loadData.m
@@ -15,6 +15,8 @@
 %                                     temperature over the 9 holes;
 %   'dataIndex'	- index of the holes to be used, 0 means to use the average
 %                 date;
+%   'timePeriod'- part of t_data are taken into account. [0,1] indicates
+%                 the full data piece;
 %   'timeScaling'- The scaling factor of time unit, currently using (days).
 % The return values:
 %   't_data'	- time series;
@@ -22,34 +24,43 @@
 %   'T_data'	- temperature measurements.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Author: Cheng Gong
-% Date: 2018-01-25
+% Date: 2018-02-01
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [t_data, z_data, T_data] = loadData(data, dataIndex, timeScaling)
+function [t_data, z_data, T_data, indt] = loadData(data, dataIndex, timePeriod, timeScaling)
     % optional input
-    if nargin < 3
+    if nargin < 4
         timeScaling = 1;
-        if nargin < 2
-            dataIndex = 0;
+        if nargin < 3
+            timePeriod = [0, 0.6];
+            if nargin < 2
+                dataIndex = 0;
+            end
         end
     end
 
     % Load Measurements
     if (dataIndex > 0) && (dataIndex < length(data.T_i))
         % Take individual measurements
-        % Time vector (row)
-        t_data = data.t'  .* timeScaling;
         % Position vector (column)
         z_data = data.z_i{dataIndex};
         % Temperature
         T_data = data.T_i{dataIndex};
     else
         % Take the averaged value
-        % Time vector (row)
-        t_data = data.t'  .* timeScaling;
         % Position vector (column)
         z_data = data.z_a;
         % Temperature
         T_data = data.T_a;        
+    end
+    
+    % Time vector (row)
+    t_data = data.t'  .* timeScaling;
+    indt = 1:length(t_data);
+        
+    % Cut data in time series
+    if (timePeriod(1) > 0) || (timePeriod(2) < 1)
+        cutRange = interp1([0, 1],[min(t_data), max(t_data)], timePeriod);
+        [T_data, t_data, indt] = cutData(T_data, t_data, cutRange, 1);
     end
 end
