a4d7b7d634f0a4d3a49ff599daa986fd37716b4a
Cheng Gong <cheng.gong@it.uu.se>
2018-02-01 19:28:50 +0100

    add cut time series

--- cutData.m
+++ cutData.m
@@ -1,8 +1,19 @@
-function [T_data, z_data, indCutZ] = cutData(T_data, z_data, z)
-    zminInd = find(z_data >= z(1), 1, 'first');
-    zmaxInd = find(z_data <= z(2), 1, 'last');
+function [T_data, cord_data, indCut] = cutData(T_data, cord_data, cutRange, timeFlag)
+    if nargin < 4
+        timeFlag = 0;
+    end
     
-    indCutZ = zminInd:zmaxInd;
-    z_data = z_data(indCutZ);
-    T_data = T_data(indCutZ, :);
+    minInd = find(cord_data >= cutRange(1), 1, 'first');
+    maxInd = find(cord_data <= cutRange(2), 1, 'last');
+    
+    indCut = minInd:maxInd;
+    cord_data = cord_data(indCut);
+    if (~timeFlag) 
+        % cut in space 
+        T_data = T_data(indCut, :);
+    else
+        % cut in time 
+        T_data = T_data(:, indCut);
+    end
+    
 end
