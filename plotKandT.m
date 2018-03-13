19e65f6f171de21631bbc252a67ee4461678440d
Cheng Gong <cheng.gong@it.uu.se>
2018-03-13 12:18:07 +0100

    add plot Ak from data

--- plotKandT.m
+++ plotKandT.m
@@ -1,4 +1,4 @@
-% Script for plot optimal K, error bar and T_data
+% Script for plot optimal K, error bar and T_data, Az and Az*R
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Author: Cheng Gong
 % Date: 2018-03-13
@@ -6,9 +6,13 @@
 
 clear
 close all
+
 %% options
 plotAz = 0;
 saveAz = 0;
+plotOptK = 0;
+plotAk = 0;
+saveAk = 0;
 
 %% Load data
 % Predefined parameters
@@ -54,53 +58,53 @@ for i = 1:2: Nt-1
     KMesh(:, ind) = tempK;
 end
 
-%% Plot K
-figure('pos',[0 0 900 600])
-subplot(2, 1, 1)
-surf(tMesh, zMesh, KMesh)
-hold on
-view(2)
-shading interp;
-colorbar
-colormap(jet)
-axis tight
-% caxis([-20, -2]);
-xlabel('t (days)')
-ylabel('z')
-grid off
-ylim([-2,8]);
-title('Optimal K')
-caxis([0, 2]);
-
-%% Plot data
-subplot(2, 1, 2);
-for i = 1: length(yearIndex)
-    data = LF{yearIndex(i)}.T;
-    [t_data, z_data, T_data, ~] = loadData(data);
-    % mesh for measurment
-    t_data = t_data/24/3600 - LF{1}.T.t(1)+1;
-    z_data = z_offset(i) + z_data;
-    
-    [X_data, Y_data] = meshgrid(t_data, z_data);
-    
-    subplot(2, 1, 2)
-    surf(X_data, Y_data, T_data)
+%% Plot Opt K and data
+if plotOptK
+    figure('pos',[0 0 900 600])
+    subplot(2, 1, 1)
+    surf(tMesh, zMesh, KMesh)
     hold on
+    view(2)
+    shading interp;
+    colorbar
+    colormap(jet)
+    axis tight
+    % caxis([-20, -2]);
+    xlabel('t (days)')
+    ylabel('z')
+    grid off
+    ylim([-2,8]);
+    title('Optimal K')
+    caxis([0, 2]);
+    
+    subplot(2, 1, 2);
+    for i = 1: length(yearIndex)
+        data = LF{yearIndex(i)}.T;
+        [t_data, z_data, T_data, ~] = loadData(data);
+        % mesh for measurment
+        t_data = t_data/24/3600 - LF{1}.T.t(1)+1;
+        z_data = z_offset(i) + z_data;
+        
+        [X_data, Y_data] = meshgrid(t_data, z_data);
+        
+        subplot(2, 1, 2)
+        surf(X_data, Y_data, T_data)
+        hold on
+    end
+    view(2)
+    shading interp;
+    colorbar
+    colormap(jet)
+    axis tight
+    caxis([-20, -2]);
+    grid off
+    xlabel('t (days)')
+    ylabel('z')
+    xlim([0,max(t_cord)]);
+    ylim([-2,8]);
+    title('Temperature measurements')
 end
-view(2)
-shading interp;
-colorbar
-colormap(jet)
-axis tight
-caxis([-20, -2]);
-grid off
-xlabel('t (days)')
-ylabel('z')
-xlim([0,max(t_cord)]);
-ylim([-2,8]);
-title('Temperature measurements')
-
-%%
+%% Plot Az and Az*R
 if plotAz
     for i = 1: length(yearIndex)
         for j = 1: length(timePeriods{yearIndex(i)})
@@ -150,6 +154,40 @@ if plotAz
             if saveAz
                 print(fig, ['Figures/Sensitivity_z', num2str(NK),'_',daytemp(1,:)], '-dpng', '-r600');
             end
+        end
+    end
+end
+
+%% Plot AK
+if plotAk
+    for k = 1: NK
+        fig = figure('pos',[0 0 900 300]);
+        for i = 1: length(yearIndex)
+            for j = 1: length(timePeriods{yearIndex(i)})
+                if k <= size(weightedAK{i,j}, 1)
+                    t = (t_cell{i,j}-t_cell{1,1}(1))/24/3600+1;
+                    z = z_cell{i,j};
+                    Ak = reshape(weightedAK{i,j}(k,:), length(z), length(t));
+                    [tmesh, zmesh] = meshgrid(t,z);
+                    surf(tmesh, zmesh, Ak)
+                    hold on
+                    view(2)
+                    shading interp;
+                    colorbar
+                    colormap(jet)
+                    axis tight
+                    xlabel('t (days)')
+                    ylabel('z')
+                    grid off
+                    ylim([-2,8]);
+                    title(['$A_{K',num2str(k),'}$'], 'Interpreter','latex')
+                    caxis([-5e-3, 5e-3]);
+                    ylim([1,8])
+                end
+            end
+        end
+        if saveAk
+            print(fig, ['Figures/Sensitivity_AK_Nk', num2str(NK), '_K', num2str(k) ], '-dpng', '-r600');
         end
     end
 end
